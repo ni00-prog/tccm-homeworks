b9446c78e6b9a64932975dd4d5d06e9cca44dbb7
ni00-prog <nicolepotenz@gmail.com>
2025-01-02 11:35:20 +0100

    Right results for hf energy

--- project1/src/main.c
+++ project1/src/main.c
@@ -1,22 +1,52 @@
+// project 1 - hf and MP2 energy calculation 
 #include <stdio.h>
 #include <stdlib.h>
 #include <trexio.h>
 #include <stdint.h>
 
+// function to find an integral in the sparse storage
+double find_integral(int i, int j, int k, int l, int64_t n_integrals, 
+                    int32_t* index, double* value) {
+   	for (int64_t n = 0; n < n_integrals; n++) {		//check all permutations
+        int idx_i = index[4*n];
+        int idx_j = index[4*n+1];
+        int idx_k = index[4*n+2];
+        int idx_l = index[4*n+3];
+        
+        if ((idx_i == i && idx_j == j && idx_k == k && idx_l == l) ||  //check equivalent permutations
+            (idx_i == i && idx_j == l && idx_k == k && idx_l == j) ||
+            (idx_i == k && idx_j == j && idx_k == i && idx_l == l) ||
+            (idx_i == k && idx_j == l && idx_k == i && idx_l == j) ||
+            (idx_i == j && idx_j == i && idx_k == l && idx_l == k) ||
+            (idx_i == j && idx_j == k && idx_k == l && idx_l == i) ||
+            (idx_i == l && idx_j == i && idx_k == j && idx_l == k) ||
+            (idx_i == l && idx_j == k && idx_k == j && idx_l == i)) {
+            return value[n];
+        }
+    }
+    return 0.0; 
+}
+
 double calculate_hf_energy(trexio_t* file) {
     trexio_exit_code rc;
     double nuclear_repulsion;
-    int32_t mo_num, n_electrons;
+    int32_t mo_num, n_up;
     double *one_e_ints;
     
+    //read nuclear repulsion
     rc = trexio_read_nucleus_repulsion(file, &nuclear_repulsion);
     printf("Nuclear repulsion energy: %.7f\n", nuclear_repulsion);
     
+    //number of electrons 
+    rc = trexio_read_electron_up_num(file, &n_up);
+    int n_electrons = 2 * n_up; 				 // Total n. electrons for closed shell
+    
+    //number of MOs
     rc = trexio_read_mo_num(file, &mo_num);
-    rc = trexio_read_electron_up_num(file, &n_electrons);
     printf("Number of electrons: %d\n", n_electrons);
     printf("Number of MOs: %d\n", mo_num);
     
+    //read one-electron integrals
     one_e_ints = malloc(mo_num * mo_num * sizeof(double));
     if (one_e_ints == NULL) {
         fprintf(stderr, "Malloc failed for one-electron integrals\n");
@@ -24,53 +54,42 @@ double calculate_hf_energy(trexio_t* file) {
     }
     rc = trexio_read_mo_1e_int_core_hamiltonian(file, one_e_ints);
 
+    //read two-electron integrals
     int64_t n_integrals;
     rc = trexio_read_mo_2e_int_eri_size(file, &n_integrals);
     printf("Number of two-electron integrals: %lld\n", n_integrals);
     
     int32_t* index = malloc(4 * n_integrals * sizeof(int32_t));
-    if (index == NULL) {
-        fprintf(stderr, "Malloc failed for index\n");
-        free(one_e_ints);
-        exit(1);
-    }
-    
     double* value = malloc(n_integrals * sizeof(double));
-    if (value == NULL) {
-        fprintf(stderr, "Malloc failed for value\n");
+    if (index == NULL || value == NULL) {
+        fprintf(stderr, "Malloc failed for two-electron integrals\n");
         free(one_e_ints);
         free(index);
+        free(value);
         exit(1);
     }
     
     int64_t buffer_size = n_integrals;
     rc = trexio_read_mo_2e_int_eri(file, 0, &buffer_size, index, value);
     
+    //calculate energy components
     double energy = nuclear_repulsion;
     double one_e_contribution = 0.0;
     
-    for (int i = 0; i < n_electrons; i++) {
-        one_e_contribution += 2.0 * one_e_ints[i * mo_num + i];
+    //one-electron contribution 
+    for (int i = 0; i < n_up; i++) {
+        one_e_contribution += 2.0 * one_e_ints[i * mo_num + i];   //factor 2 for closed shell
     }
     energy += one_e_contribution;
     printf("One-electron contribution: %.7f\n", one_e_contribution);
     
+    //two-electron contribution
     double two_e_contribution = 0.0;
-    for (int i = 0; i < n_electrons; i++) {
-        for (int j = 0; j < n_electrons; j++) {
-            for (int64_t n = 0; n < buffer_size; n++) {
-                int k = index[4*n+2];
-                int l = index[4*n+3];
-                
-                if (index[4*n] == i && index[4*n+1] == j) {
-                    if (k == i && l == j) {
-                        two_e_contribution += 2.0 * value[n];
-                    }
-                    if (k == j && l == i) {
-                        two_e_contribution -= value[n];
-                    }
-                }
-            }
+    for (int i = 0; i < n_up; i++) {
+        for (int j = 0; j < n_up; j++) {
+            double Jij = find_integral(i, j, i, j, n_integrals, index, value);
+            double Kij = find_integral(i, j, j, i, n_integrals, index, value);
+            two_e_contribution += 2.0 * Jij - Kij;
         }
     }
     energy += two_e_contribution;
@@ -97,5 +116,3 @@ int main() {
     trexio_close(file);
     return 0;
 }
-
-//the hf energy is wrong. 
